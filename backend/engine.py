1bdc6f122345a84354f7469547e2f5b3f4ff0d08
Adrian Dobkowski <adrian.dobkowski2c@gmail.com>
2025-01-06 13:36:33 +0100

    fix

--- backend/engine.py
+++ backend/engine.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from db_access import database_url
-
+database_url = "sqlite:///database.db"
 engine = create_engine(database_url)
 Session = sessionmaker(bind=engine)
 
