0814149b430235f0d799750b872d9ee0631e584c
Adrian Dobkowski <adrian.dobkowski2c@gmail.com>
2025-01-06 20:47:53 +0100

    working on the account page

---
+++ frontend/src/components/AccountPage.tsx
@@ -0,0 +1,7 @@
+import axios from "axios"
+
+import AccountDetails from "./register";
+export default AccountPage(): JSX.Element {
+    const [details, setDetails] = useState<AccountDetails>()
+
+}
--- frontend/src/components/register.tsx
+++ frontend/src/components/register.tsx
@@ -1,22 +1,22 @@
 import { useState } from "react";
 import axios from 'axios';
 import HomePage from "./HomePage";
-interface AccountDetails {
+export default interface AccountDetails {
     username: string,
     password: string,
     name: string,
     surname: string,
-    email: string, 
+    email: string,
     phone_number: string
 }
 
 export default function Register(): JSX.Element {
     const url = "http://127.0.0.1:5000/create_account"
-    const [accountDetails, setAccountDetails] = useState<AccountDetails>({username:'', password:'', name:'', surname:'', email:'', phone_number:''})
+    const [accountDetails, setAccountDetails] = useState<AccountDetails>({ username: '', password: '', name: '', surname: '', email: '', phone_number: '' })
     const [isCreated, setIsCreated] = useState<boolean>(false)
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
         event.preventDefault();
-        if (accountDetails.username && accountDetails.password && accountDetails.name && 
+        if (accountDetails.username && accountDetails.password && accountDetails.name &&
             accountDetails.surname && accountDetails.email && accountDetails.phone_number) {
             axios.post(url, accountDetails)
                 .then((response) => {
@@ -39,34 +39,40 @@ export default function Register(): JSX.Element {
                 });
         }
     };
-    if (!isCreated){
-    return (
-        <form onSubmit={(event) => handleSubmit(event)}>
-            <input type="text" placeholder="username" value={accountDetails?.username} onChange={(event) => setAccountDetails((previous) => ({
-                ...previous,
-                username: event.target.value}))}/>
+    if (!isCreated) {
+        return (
+            <form onSubmit={(event) => handleSubmit(event)}>
+                <input type="text" placeholder="username" value={accountDetails?.username} onChange={(event) => setAccountDetails((previous) => ({
+                    ...previous,
+                    username: event.target.value
+                }))} />
 
-            <input type="password" placeholder="password" value={accountDetails?.password} onChange={(event) => setAccountDetails((previous) => ({
-                ...previous,
-                password: event.target.value}))}/>
-            <input type="text" placeholder="name" value={accountDetails?.name} onChange={(event) => setAccountDetails((previous) => ({
-                ...previous,
-                name: event.target.value}))}/>
-            <input type="text" placeholder="surname" value={accountDetails?.surname} onChange={(event) => setAccountDetails((previous) => ({
-                ...previous,
-                surname: event.target.value}))}/>
-            <input type="text" placeholder="email" value={accountDetails?.email} onChange={(event) => setAccountDetails((previous) => ({
-                ...previous,
-                email: event.target.value}))}/>
-            <input type="text" placeholder="phone_number" value={accountDetails?.phone_number} onChange={(event) => setAccountDetails((previous) => ({
-                ...previous,
-                phone_number: event.target.value}))}/>
+                <input type="password" placeholder="password" value={accountDetails?.password} onChange={(event) => setAccountDetails((previous) => ({
+                    ...previous,
+                    password: event.target.value
+                }))} />
+                <input type="text" placeholder="name" value={accountDetails?.name} onChange={(event) => setAccountDetails((previous) => ({
+                    ...previous,
+                    name: event.target.value
+                }))} />
+                <input type="text" placeholder="surname" value={accountDetails?.surname} onChange={(event) => setAccountDetails((previous) => ({
+                    ...previous,
+                    surname: event.target.value
+                }))} />
+                <input type="text" placeholder="email" value={accountDetails?.email} onChange={(event) => setAccountDetails((previous) => ({
+                    ...previous,
+                    email: event.target.value
+                }))} />
+                <input type="text" placeholder="phone_number" value={accountDetails?.phone_number} onChange={(event) => setAccountDetails((previous) => ({
+                    ...previous,
+                    phone_number: event.target.value
+                }))} />
 
-            <button type="submit">Create Account</button>
-        </form>
+                <button type="submit">Create Account</button>
+            </form>
         )
     } else {
         alert("Successful account creation")
-        return <HomePage/>
+        return <HomePage />
     }
 }
